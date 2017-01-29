fc54a99672bfea335c2400ab8cf9317254b22604
AlexRatmansky <alexratmansky@yandex.ru>
2017-01-29 23:42:08 +0300

    добавляет направляющие линейки

--- js/vue-script.js
+++ js/vue-script.js
@@ -1,15 +1,30 @@
 document.body.addEventListener('touchmove', e => e.preventDefault());
 
-Vue.component('guide-item', {
+Vue.component('view-template', {
   template: '#view-template',
-  props: ['xPos', 'yPos']
+  props: ['xPos', 'yPos', 'guides']
+});
+
+Vue.component('guide-item', {
+  template: '#view-guide',
+  props: ['isVertical', 'isHorisontal', 'yPos', 'xPos']
 });
 
 new Vue({
   el: '#app',
   data: {
-    xPos: "0",
-    yPos: "0"
+    xPos: '0',
+    yPos: '0',
+    guides: [
+      {
+        isVertical: false,
+        isHorisontal: true
+      },
+      {
+        isVertical: true,
+        isHorisontal: false
+      }
+    ]
   },
   methods: {
     onDrag: function (e) {
