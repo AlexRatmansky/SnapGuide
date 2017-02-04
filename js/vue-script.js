d4c971e2ddd27f0baa7d1fef736221cb580954e8
AlexRatmansky <alexratmansky@yandex.ru>
2017-02-04 21:51:04 +0300

    Добавляет декоратор тормозилку для передачи параметров события курсора

--- js/vue-script.js
+++ js/vue-script.js
@@ -31,6 +31,42 @@ vv = new Vue({
   }
 });
 
-document.onmousemove = function(e){
-  vv.event = e;
+document.onmousemove = function (e) {
+  passEventData(e);
 };
+
+passEventData = throttle(passEventData, 20);
+
+function passEventData(eventData) {
+  vv.event = eventData;
+}
+
+function throttle(func, ms) {
+
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
