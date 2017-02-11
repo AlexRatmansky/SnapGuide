36856b3835b4a0699ef42f5af8fbcea8043e14da
AlexRatmansky <alexratmansky@yandex.ru>
2017-02-11 16:38:18 +0300

    переносит стили в модули

--- snapMeasure/js/components/GuideItem.vue
+++ snapMeasure/js/components/GuideItem.vue
@@ -11,3 +11,27 @@
     props: ['isVertical', 'yPos', 'xPos']
   };
 </script>
+
+<style>
+  .guide {
+    display: block;
+    position: absolute;
+    background-color: green;
+    top: 50%;
+    left: 50%;
+    z-index: 100;
+    pointer-events: none;
+  }
+
+  .guide.-v {
+    width: 1px;
+    top: 0;
+    height: 100%;
+  }
+
+  .guide.-h {
+    left: 0;
+    height: 1px;
+    width: 100%;
+  }
+</style>
--- snapMeasure/js/components/ViewElement.vue
+++ snapMeasure/js/components/ViewElement.vue
@@ -15,3 +15,15 @@
     props: ['elementProps']
   };
 </script>
+
+<style>
+  .element {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 0;
+    background: rgba(255, 0, 0, 0.3);
+    pointer-events: none;
+  }
+</style>
