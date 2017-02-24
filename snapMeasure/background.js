c5aeeaaf721516d3a0af2f9be3f07a9cee7b6433
AlexRatmansky <alexratmansky@yandex.ru>
2017-02-25 00:12:52 +0300

    Ограничивает количество внедрений скрипта на страницу

--- snapMeasure/background.js
+++ snapMeasure/background.js
@@ -1,3 +1,8 @@
-chrome.browserAction.onClicked.addListener(function () {
-  chrome.tabs.executeScript(null, {file: 'inpage.js'});
+var a = true;
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  if (a) {
+    chrome.tabs.executeScript(tab.id, {file: 'inpage.js'});
+    a = false;
+  }
 });
