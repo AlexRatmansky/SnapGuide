d02a241fe3e16700dfa217514ad1a58956f86133
AlexRatmansky <alexratmansky@yandex.ru>
2017-03-04 10:51:03 +0300

    Убирает все лишнее

--- snapMeasure/background.js
+++
@@ -1,8 +0,0 @@
-let isRunning = false;
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-  if (!isRunning) {
-    chrome.tabs.executeScript(tab.id, {file: 'inpage.js'});
-    isRunning = true;
-  }
-});
