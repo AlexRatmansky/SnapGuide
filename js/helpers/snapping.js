ad6bbbf6963414472b1f8d79d7ded2027039d2b9
AlexRatmansky <alexratmansky@yandex.ru>
2017-04-30 18:38:18 +0300

    Добавляет возможность прилипать к базовой линии первой строки текста

--- js/helpers/snapping.js
+++ js/helpers/snapping.js
@@ -55,6 +55,11 @@ export function checkSnap(element, xPos, yPos) {
     isSnapped = true;
   }
 
+  if (getBaselineY(element)) {
+    newYPos = getBaselineY(element) - bodyRect.top;
+    isSnapped = true;
+  }
+
   function checkTop(top, y) {
     return y <= top + snapFactor && y >= top - snapFactor
   }
@@ -71,9 +76,49 @@ export function checkSnap(element, xPos, yPos) {
     return y >= bottom - snapFactor && y <= bottom + snapFactor
   }
 
+
   return {
     xPos: newXPos,
     yPos: newYPos,
     isSnapped: isSnapped
   };
+}
+
+function getBaselineY(target) {
+  // TODO: подумать что можно сделать с таблицами>
+  if (!target.hasChildNodes() || target.tagName === 'TABLE') {
+    return null;
+  }
+
+  let children = target.childNodes;
+  let child = null;
+
+  for (let i = 0; i < children.length; ++i) {
+    if (children[i].nodeType === Node.TEXT_NODE) {
+      child = children[i];
+      break;
+    }
+  }
+
+  if (child === null) {
+    return null;
+  }
+
+  let txt = document.createElement('span');
+
+  txt.style.setProperty('display', 'inline-block', 'important');
+  txt.style.setProperty('position', 'static', 'important');
+  txt.style.setProperty('height', '1px', 'important');
+  txt.style.setProperty('width', '1px', 'important');
+  txt.style.setProperty('padding', '0', 'important');
+  txt.style.setProperty('margin', '0', 'important');
+  txt.style.setProperty('background-color', 'green', 'important');
+
+  target.insertBefore(txt, child);
+
+  let y = txt.getBoundingClientRect().top + 1;
+
+  target.removeChild(txt);
+
+  return y;
 }
