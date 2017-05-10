ef9a80f84dcb36832861bbd4a6c1a427f6ff8e63
AlexRatmansky <alexratmansky@yandex.ru>
2017-05-10 15:01:50 +0300

    Правит конфиг webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,6 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          },
-          // other vue-loader options go here
           cssModules: {
             localIdentName: '[local]__[hash:base64:5]',
             camelCase: true
@@ -43,7 +35,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: '[name]-[hash].[ext]'
         }
       }
     ]
