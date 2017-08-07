1c54a46eb4dac717a2ff91168dfd285810b06387
Александр Ратманский <alexratmansky@yandex.ru>
2017-08-07 18:04:44 +0400

    Adds shortcut for launching extension

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 let isRunning = false;
 
+chrome.commands.onCommand.addListener(function (command) {
+  // Call 'update' with an empty properties object to get access to the current
+  // tab (given to us in the callback function).
+  chrome.tabs.update({}, function (tab) {
+    if (command === 'toggle-snap-guide') {
+      chrome.tabs.executeScript(tab.id, {file: 'inpage.js'});
+      isRunning = true;
+    }
+  });
+});
+
 chrome.browserAction.onClicked.addListener(function (tab) {
   if (!isRunning) {
     chrome.tabs.executeScript(tab.id, {file: 'inpage.js'});
