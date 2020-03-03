ea343f3852f5b0cbf16c8781bc685fb96aaaeb77
AlexRatmansky <alexratmansky@yandex.ru>
2020-03-03 23:17:10 +0300

    Направления

--- src/components/PointerGuide/index.tsx
+++ src/components/PointerGuide/index.tsx
@@ -8,10 +8,51 @@ export { PointerGuide };
 const PointerGuide: FC = () => {
   const { crossPos } = useSelector((store: Store) => store);
 
+  let direction = getDirection(crossPos);
+
   return (
     <>
-      <Guide isVertical={true} position={crossPos.x} crossGuide={true} />
-      <Guide isVertical={false} position={crossPos.y} crossGuide={true} />
+      {['left', 'right'].includes(direction) ? (
+        <Guide isVertical={true} position={crossPos.x} crossGuide={true} />
+      ) : (
+        <Guide isVertical={false} position={crossPos.y} crossGuide={true} />
+      )}
     </>
   );
+};
+
+let oldX = 0;
+let oldY = 0;
+let oldDirection = 0;
+
+const getDirection = crossPos => {
+  let direction;
+  let directionX;
+  let directionY;
+  let diffX = 0;
+  let diffY = 0;
+
+  if (crossPos.x < oldX) {
+    directionX = 'left';
+    diffX = oldX - crossPos.x;
+  } else if (crossPos.x > oldX) {
+    directionX = 'right';
+    diffX = crossPos.x - oldX;
+  }
+
+  if (crossPos.y < oldY) {
+    directionY = 'top';
+    diffY = oldY - crossPos.y;
+  } else if (crossPos.y > oldY) {
+    directionY = 'bottom';
+    diffY = crossPos.y - oldY;
+  }
+
+  oldX = crossPos.x;
+  oldY = crossPos.y;
+
+  direction = diffX > diffY ? directionX : diffY > diffX ? directionY : oldDirection;
+  oldDirection = direction;
+
+  return direction;
 };
